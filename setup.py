e87f0926cc2ef1396852b75bb4a4953503b38a11
Danil <danil.shapoval21@gmail.com>
2022-03-17 16:50:57 +0530

    update version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="b2binpay-python",
-    version="0.0.3",
+    version="0.0.4",
     author="allkap",
     author_email="author@example.com",
     description="Library for working with b2binpay api",
